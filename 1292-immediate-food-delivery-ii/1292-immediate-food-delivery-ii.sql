5943fc1ff5ba8bf4a42f0639ce823f02f48c36c5
HellSkk <139304083+HellSkk@users.noreply.github.com>
2025-06-05 09:29:12 +0900

    Time: 627 ms (67.16%), Space: 0B (100%) - LeetHub

--- 1292-immediate-food-delivery-ii/1292-immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/1292-immediate-food-delivery-ii.sql
@@ -3,12 +3,9 @@
 select round(sum(case when schedule = 'immediate' then 1 else 0 end) / count(*) * 100, 2) as immediate_percentage
 from (
 select customer_id
-    ,   min(order_date) as order_date
-    ,   min(customer_pref_delivery_date) as customer_pref_delivery_date
     ,   case
             when min(order_date) - min(customer_pref_delivery_date) = 0 then "immediate"
             else "scheduled" end as schedule
 from delivery
 group by customer_id
-order by order_date
 ) as a
